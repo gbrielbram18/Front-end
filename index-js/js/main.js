f44fe78844f99c4e564eee4ce343d8598ba66332
gbrielbram18 <biel300816@gmail.com>
2025-09-03 20:46:04 -0300

    adicionando exemplos e introdução de JS

---
+++ index-js/js/main.js
@@ -0,0 +1,6 @@
+//para aparecer uma mensagem na tela 
+alert("Hello World!");
+
+//para aparecer no console
+console.log("Ola Mundo");
+
---
+++ index-js/js/maindois.js
@@ -0,0 +1,6 @@
+//decalrando uma variavel
+let minhaVariavel;
+minhaVariavel =10;
+
+console.log(minhaVariavel);
+aleta()
